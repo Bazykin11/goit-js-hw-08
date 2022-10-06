3a0936fbbfddf2abef89370e08daa2b50e985ec0
Bazykin11 <103375706+Bazykin11@users.noreply.github.com>
2022-10-06 23:00:03 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,21 +9,18 @@ form.addEventListener('submit', onSubmit);
 
 
 const obj = JSON.parse(localStorage.getItem('feedback-form-state'))|| {};
-// console.dir(form.elements.value);
 form.elements.email.value = obj.email || '';
 form.elements.message.value = obj.message || '';
 
 function onForm (e){
-    // console.log(e.target.value);
-    // console.dir(e.target.name);
     obj[e.target.name] = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(obj));
     // console.log(obj);
-    localStorage.setItem('feedback-form-state', JSON.stringify(obj))
 }
 
 function onSubmit(e) {
     e.preventDefault();
-    console.log('Отправка');
+    console.log(obj);
     e.currentTarget.reset();
     localStorage.removeItem('feedback-form-state');
 }
